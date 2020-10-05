6a2e68591dfdb3dc632438707d29bf8621d93d49
Walace Paz <walacepaz11@gmail.com>
2020-10-05 20:26:54 -0300

    criado route.php e migrado rotas para la

---
+++ route.php
@@ -0,0 +1,37 @@
+<?php
+
+use Lib\Http\CreateRoute as Route;
+use Lib\Http\Request;
+
+Route::get("/", function(){
+    echo "/";
+});
+
+Route::get("branco", function(){
+});
+
+Route::get("get", function(){
+    echo "get";
+});
+
+Route::get("get/request", function(Request $request){
+    echo "request";
+});
+
+Route::get("get/post/form", function(){
+    echo "
+        <form action='" . Route::string("post") . "' method='post'>
+            <input name='teste'>
+            <button type='submit'>Envar</button>
+        </form>
+    ";
+});
+
+Route::post("post", function(Request $request){
+    echo $request->post()->teste;
+    echo "<br><a href='". Route::string("get/post/form") . "'>voltar</a>";
+});
+
+Route::get("controller/{param1}/{param2}", "Index", "index");
+
+Route::get("controller", "Home", "index");
